c9482190006a440d6e03a0e7d5e84888fef71544
omerb304 <omerbachartadam@gmail.com>
2019-03-21 19:10:20 +0200

    Past updates

--- handleFb.js
+++ handleFb.js
@@ -3,6 +3,32 @@
  */
 
 
+/**
+ * Handling facebook page, its' configuration and 
+ * additional settings, like checking LTR/RT or feed language
+ *
+ * @void
+ */
+ var getFbSettings = () => {
+    const writingDirection = $("body").attr("dir");
+    const currentUrl = window.location.href.toString();
+    
+    let bodyClasses = $("body").attr("class").split(" ");
+    //console.log(bodyClasses);
+    var language = "unknown";
+    bodyClasses.forEach((val) => {
+        if(val.toLowerCase().indexOf('locale') !== -1){
+            language = val;
+        }
+    });
+    
+    return { 
+        writingDirection : writingDirection,
+        currentUrl : currentUrl,
+        language : language
+    };
+
+ };
 
 /**
  * Returns all the new posts in feed
@@ -10,12 +36,12 @@
  */
 var getPosts = () => {
     let postsContainers = document.querySelectorAll(
-        "div[data-testid='fbfeed_story']:not(.feedButtonSuccess)"
+        "div[data-testid='fbfeed_story']:not(.feedButtonSuccess):not(.feedButtonFail)"
     );
 
     if (postsContainers.length === 0) {
         // Another options
-        postContainers = document.querySelectorAll("*[id*=hyperfeed_story_id]:not(.feedButtonSuccess");
+        postContainers = document.querySelectorAll("*[id*=hyperfeed_story_id]:not(.feedButtonSuccess):not(.feedButtonFail)");
         if (!postsContainers || postsContainers.length === 0) {
             return false;
         }
@@ -37,6 +63,7 @@ var getPosts = () => {
  * @param {String} postLink - post URL
  * @return {Object}
  */
+
 var createButton = (postId,postLink) => {
     let button = document.createElement("span");
     button.className = "bookmark add";
@@ -54,13 +81,82 @@ var createButton = (postId,postLink) => {
  * @param {Object} space - space element ("•")
  * @void
  */
-var addButton = (ele,postId,postLink,space) => {
+var addButton = (ele,postId,postLink) => {
     // Validate element don't has the button
     if(!$(ele).has(".bookmark").length){
-        
+
         $(ele).append('<span class="_6spk" role="presentation" aria-hidden="true"> · </span>');
         $(ele).append(createButton(postId,postLink));
+    }
+    else{
+        console.log("Already added");
     }
     
 }; 
 
+
+/**
+ * Place buttons on specific element
+ * @param {Object} ele - element object to place buttons on 
+ * @return {Boolean}
+ */
+var placeButtons = (ele) => {
+    let doneList = [];
+    let failList = [];
+
+    let postId = $(ele).find("input[name='ft_ent_identifier']").first().attr("value");
+    console.log(postId);
+    if (!postId) {
+        // TODO - find a unique identifier to those posts
+        postId = "unknown";
+        return false;
+    }
+    if (!doneList.includes(ele) && !failList.includes(ele) && postId !== "unknown") {
+
+        let postUrl = $(ele).find("div[id*='feed_subtitle'] a[target]").first().attr("href");
+
+        let subtitle_container = null;
+        if ($(ele).has("div[id*='feed_subtitle']").length) {
+            // Normal post
+            // console.log("2");
+            subtitle_container = $(ele).find("div[id*='feed_subtitle']").first();
+            // console.log(subtitle_container);
+        } else if ($(ele).has(".userContentWrapper span.uiStreamAdditionalLogging").length) {
+            // May be an ad
+            // console.log("1");
+            subtitle_container = $(ele).find("span.uiStreamAdditionalLogging").first().parent();
+            // console.log(subtitle_container);
+        }
+        else {
+            // Unknown post type
+            console.log("Bookmark cant be added to postid '" + postId + "'.");
+            return false;
+
+        }
+        console.log(subtitle_container);
+        if (subtitle_container && !$(ele).has(".bookmark").length) {
+            // let space = $(ele).find("div[id*='feed_subtitle'] span:contains(' · ')").first();
+            // space.addClass("spaceFeedNotes");
+            // console.log(subtitle_container);
+
+            addButton(subtitle_container, postId, postUrl);
+
+            if (!$(ele).has(".bookmark").length) {
+                console.log("Failed " + postId);
+                return false;
+            }
+            else {
+                $(ele).addClass("feedButtonSuccess");
+                console.log("Success " + postId);
+                doneList.push(postId);
+                return true;
+            }
+        }
+        doneList.push(postId);
+    } else {
+        failList.push(ele);
+        $(ele).addClass("feedButtonFail");
+
+        return false;
+    }
+};
--- main.js
+++ main.js
@@ -7,105 +7,97 @@
 var main = () => {
     let a = {};
 
+    a.settings = {};
+
+    const posts_containers = [];
+
     /**
      * Adds listeners to doucment elements
      * @void
      */
+
     var addListeners = () => {
 
-        jq('div[id="content"]').on("DOMNodeInserted", () => {
-            // jq('div[data-testid="fbfeed_story"]').on("DOMNodeInserted", () => {
-            //     console.log("HEY HEY HEY");
-            //     handleNewPosts();
-            // });
-            let events = getEvents($('div[data-testid="fbfeed_story"]').first()[0]);
-            if (!events) {
-                jq('div[data-testid="fbfeed_story"]').on("DOMNodeInserted", () => {
-                    handleNewPosts();
-                });
-            }
+        // jq('div[id="content"]').on("DOMNodeInserted", () => {
+        //     // jq('div[data-testid="fbfeed_story"]').on("DOMNodeInserted", () => {
+        //     //     console.log("HEY HEY HEY");
+        //     //     handleNewPosts();
+        //     // });
+        //     let events = getEvents($('div[data-testid="fbfeed_story"]').first());
+        //     if (!events) {
+        //         jq('div[data-testid="fbfeed_story"]').on("DOMNodeInserted", (evt) => {
+        //             handleNewPost(evt);
+        //         });
+        //     }
+        // });
+        
+        // Feed posts
+        $(document).on('DOMNodeInserted', 'div[data-testid="fbfeed_story"]', (evt) => {
+            handleInsertedNewPost(evt,'feed');
         });
 
+        // Group page posts
+        $(document).on('DOMNodeInserted', "div[id*='mall_post']:not([id*='SUGGESTED'])", (evt) => {
+            handleInsertedNewPost(evt,'group');
+        });
+
+        // Page posts
+        $(document).on('DOMNodeInserted', "#pagelet_timeline_main_column div[role='article']", (evt) => {
+            handleInsertedNewPost(evt,'page');
+        });
+
+        // Events pages posts
+        $(document).on('DOMNodeInserted', "div[id*='mall_post'][role='article'], div[id*='event_post'][role='article']", (evt) => {
+            handleInsertedNewPost(evt,'event');
+        });
     }
 
     /**
-     * Handling scrolling and additional posts on feed
+     * Handling listener of inserting new post to feed
+     * @param {Object} evt - event listener object
+     * @param {String} type - post source type (group page post, feed post)
      * @void
      */
-    var handleNewPosts = () => {
-        placeButtons();
+    var handleInsertedNewPost = (evt,type) => {
+        // Check if post has already been added
+        console.log
+        if (!posts_containers.includes(evt.currentTarget)) {
+            // Add new post to list
+            let newPost = evt.currentTarget;
+            posts_containers.push(
+                newPost
+            );
+
+            console.log("Post has been added");
+
+            // Execute actions on post
+            handlePost(newPost);
+            //console.log(newPost);
+        }
+        // else{
+        //     console.log("Post has already been added to list.");
+        // }
+
     };
 
     /**
-     * Injects buttons into facebook feed for each post
+     * Handling post container - doing all the job on the post
+     * @param {Object} postContainer - Container of post 
+     */
+    var handlePost = (postContainer) => {
+        console.log(postContainer);
+        // break;
+        placeButtons(postContainer);
+    };
+
+    /**
+     * Handling situation of page loaded - neccessary if many posts was already
+     * loaded, so DOMInserted event won't be fired.
+     * Because of that, we must handle it manually :)
+     * 
      * @void
-     * */
-    var placeButtons = () => {
-
-        /**
-         * TODO: find the differences between ads posts to regular posts
-         */
-
-        // Find the whole posts containers
-        let postsList = getPosts();
-        if (!postsList || postsList.length === 0) {
-            return;
-        }
-
-        console.log(postsList);
-
-        console.log(postsList.length + " new posts has been found");
-        let space = $("body").find("div[id*='feed_subtitle'] span:contains(' · ')").first()[0];
-        console.log(space);
-        if (!space) {
-            space = document.createElement("span");
-            space.innerHTML = " ";
-        }
-        $(space).addClass("spaceFeedNotes");
-
-        $.each(postsList, (index, ele) => {
-
-            let postId = $(ele).find("input[name='ft_ent_identifier']").first().attr("value");
-            let postUrl = $(ele).find("div[id*='feed_subtitle'] a[target]").first().attr("href");
-
-            let subtitle_container = null;
-            if ($(ele).has("div[id*='feed_subtitle']").length) {
-                // Normal post
-                // console.log("2");
-                subtitle_container = $(ele).find("div[id*='feed_subtitle']").first();
-                // console.log(subtitle_container);
-            } else if ($(ele).has(".userContentWrapper span.uiStreamAdditionalLogging").length) {
-                // May be an ad
-                // console.log("1");
-                subtitle_container = $(ele).find("span.uiStreamAdditionalLogging").first().parent();
-                // console.log(subtitle_container);
-            }
-            else {
-                // Unknown post type
-                // console.log("Bookmark cant be added to postid '" + postId + "'.");
-
-            }
-
-            if (subtitle_container && !$(ele).has(".bookmark").length) {
-                // let space = $(ele).find("div[id*='feed_subtitle'] span:contains(' · ')").first();
-                // space.addClass("spaceFeedNotes");
-                // console.log(subtitle_container);
-
-                addButton(subtitle_container, postId, postUrl, space);
-
-                if (!$(ele).has(".bookmark").length) {
-                    console.log("Failed " + postId);
-                }
-                else {
-                    $(ele).addClass("feedButtonSuccess");
-                    console.log("Success " + postId);
-                }
-            }
-
-
-
-
-        });
+     */
+    var handlePageLoad = () => {
 
     };
 
@@ -117,11 +109,15 @@ var main = () => {
                 () => {
                     console.log("Jquery has been injected.");
 
+                    // Handle configuration and settings
+                    a.settings = getFbSettings();
+
+                    console.log(a.settings);
                     // Adding Listeners
                     addListeners();
 
-                    // Placing 'add' and 'remove' buttons nearby posts
-                    placeButtons();
+                    // Handling page loaded
+                    //handlePageLoad();
                 })
             .catch(
                 (err) => {
